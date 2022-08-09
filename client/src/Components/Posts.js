fed3f6c435f9c6230708c301348baf968cc44ae0
Kaegan O'Hara <kohara321@gmail.com>
2022-08-09 13:26:14 -0600

    OAuth and Navbar

---
+++ client/src/Components/Posts.js
@@ -0,0 +1,9 @@
+
+
+function Posts(){
+    return(
+        <h1>Posts page</h1>
+    )
+}
+
+export default Posts;
---
+++ client/src/Components/logout.js
@@ -0,0 +1,23 @@
+import { GoogleLogout } from "react-google-login";
+
+const clientId = "642741965839-n4f0rl2hcrfgg1fg15ea75q3f2cdgek7.apps.googleusercontent.com"
+
+
+function Logout() {
+
+    const onSuccess = () => {
+        console.log("Log out successfull!")
+    }
+
+    return(
+        <div id="signOutButton">
+            <GoogleLogout
+                clientId={clientId}
+                buttonText={"Logout"}
+                onLogoutSuccess={onSuccess}
+            />
+        </div>
+    )
+}
+
+export default Logout;
